03635e121aa9c7dd802defbf6d5543db2abcd993
Baldnut <aaronheald181@hotmail.com>
2024-11-25 15:18:07 +0000

    Created event framework

---
+++ core/src/main/java/com/vikingz/unitycoon/events/EventManager.java
@@ -0,0 +1,12 @@
+package com.vikingz.unitycoon.events;
+
+public class EventManager {
+
+    /**
+     * Chooses a random event to run
+     */
+    public void randomEvent() {
+
+
+    }
+}
---
+++ core/src/main/java/com/vikingz/unitycoon/events/EventPopup.java
@@ -0,0 +1,25 @@
+package com.vikingz.unitycoon.events;
+
+import com.vikingz.unitycoon.menus.PopupMenu;
+
+public class EventPopup {
+
+    private PopupMenu popup;
+
+    /**
+     * Creates a new event popup
+     *
+     */
+    public void event() {
+
+    }
+
+    /**
+     * Creates a new event popup with choices
+     *
+     */
+    public void choiceEvent() {
+
+    }
+
+}
