e9ab7149c0700666829eaf7063ed3d99c061e765
Will Hall <will@limeparallelogram.uk>
2024-12-01 19:05:42 +0000

    Added file load tests

---
+++ headless/src/test/java/com/vikingz/unitycoon/headless/GameConfigTest.java
@@ -0,0 +1,65 @@
+package com.vikingz.unitycoon.headless;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.GL20;
+import com.vikingz.unitycoon.global.GameConfig;
+import com.vikingz.unitycoon.global.GameConfigManager;
+
+public class GameConfigTest {
+
+    @BeforeEach
+    public void setup() {
+        Gdx.gl = Gdx.gl20 = mock(GL20.class);
+        HeadlessLauncher.main(new String[0]);
+    }
+
+    /**
+     * Attempt to save a new value to the config file, save it and then load the saved value
+     */
+    @Test
+    public void testConfigFileLoadSave() {
+        FileHandle confFile = Gdx.files.internal("config/gameconf.bin");
+
+        // Test initial load
+        assertAll("Test that file load ok", () -> GameConfigManager.loadGameConfig());
+        float initialSoundVolume = GameConfig.getInstance().SoundVolumeValue;
+
+        // Attempt to save config to file
+        GameConfig.getInstance().SoundVolumeValue = 300;
+        assertAll("Test that file saving is ok", () -> GameConfigManager.saveGameConfig());
+        
+        assertTrue(confFile.exists(), "Check that save file has been created");
+        
+        // Load and confirm retrieved data is as expected
+        GameConfig.getInstance().SoundVolumeValue = -300;  // Set bogus value to check this gets overwritten
+
+        GameConfigManager.loadGameConfig();
+        assertEquals(300, GameConfig.getInstance().SoundVolumeValue, "Check that saved value was loaded successfully");
+
+        // Cleanup and reset sound volume to initial value
+        GameConfig.getInstance().SoundVolumeValue = initialSoundVolume;
+        GameConfigManager.saveGameConfig();
+    }
+
+    /**
+     * Confirm that a basic load operation overwrites existing stored config values
+     */
+    @Test
+    public void testConfigLoadOnly() {
+        // Load and confirm retrieved data is as expected
+        GameConfig.getInstance().SoundVolumeValue = -300;  // Set bogus value to check this gets overwritten
+
+        GameConfigManager.loadGameConfig();
+        assertNotEquals(-300, GameConfig.getInstance().SoundVolumeValue, "Check that configuration is loaded successfully");
+    }
+}
