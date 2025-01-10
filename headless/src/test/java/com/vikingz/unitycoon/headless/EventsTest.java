10db4bd5de6caff934927aa08ab1cffda917901b
Will Hall <will@limeparallelogram.uk>
2025-01-10 16:23:15 +0000

    Completed EventTests

--- headless/src/test/java/com/vikingz/unitycoon/headless/EventsTest.java
+++ headless/src/test/java/com/vikingz/unitycoon/headless/EventsTest.java
@@ -1,6 +1,8 @@
 package com.vikingz.unitycoon.headless;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -19,7 +21,11 @@ import com.vikingz.unitycoon.events.eventfiles.Event;
 import com.vikingz.unitycoon.events.eventfiles.FeeIncreaseEvent;
 import com.vikingz.unitycoon.events.eventfiles.FireEvent;
 import com.vikingz.unitycoon.events.eventfiles.FloodEvent;
+import com.vikingz.unitycoon.events.eventfiles.RosesEvent;
+import com.vikingz.unitycoon.events.eventfiles.RosesWinEvent;
 import com.vikingz.unitycoon.events.eventfiles.SponsorEvent;
+import com.vikingz.unitycoon.events.eventfiles.StrikesEvent;
+import com.vikingz.unitycoon.events.eventfiles.StrikesResolvedEvent;
 import com.vikingz.unitycoon.global.GameGlobals;
 
 public class EventsTest extends TestSuper {
@@ -55,7 +61,7 @@ public class EventsTest extends TestSuper {
         assertEquals(originalBalance+1000, GameGlobals.MONEY.getBalance(), "This event should increase balance by £1000k");
         assertEquals(originalSatisfaction, GameGlobals.SATISFACTION.getSatisfaction(), "This event should have no effect on satisfaction");
 
-        assertEquals(1, GameGlobals.EVENT.getPositiveEvents(), "Check the event is registered as positive event");
+        assertEquals(1, GameGlobals.EVENT.getPositiveEvent(), "Check the event is registered as positive event");
     }
 
     
@@ -75,7 +81,7 @@ public class EventsTest extends TestSuper {
         assertEquals(originalBalance, GameGlobals.MONEY.getBalance(), "This event should have no effect on balance");
         assertEquals(originalSatisfaction, GameGlobals.SATISFACTION.getSatisfaction(), "This event should have no effect on satisfaction");
 
-        assertEquals(1, GameGlobals.EVENT.getPositiveEvents(), "Check the event is registered as positive event");
+        assertEquals(1, GameGlobals.EVENT.getPositiveEvent(), "Check the event is registered as positive event");
     }
 
 
@@ -93,7 +99,7 @@ public class EventsTest extends TestSuper {
         assertEquals(originalBalance, GameGlobals.MONEY.getBalance(), "This event should have no effect on balance");
         assertEquals(originalSatisfaction-10, GameGlobals.SATISFACTION.getSatisfaction(), "This event should decrease satisfaction by 10%");
 
-        assertEquals(1, GameGlobals.EVENT.getNegativeEvents(), "Check the event is registered as negative event");
+        assertEquals(1, GameGlobals.EVENT.getNegativeEvent(), "Check the event is registered as negative event");
     }
 
 
@@ -126,8 +132,8 @@ public class EventsTest extends TestSuper {
         // Confirm that satisfaction is handled as expected
         assertEquals(originalSatisfaction-10, GameGlobals.SATISFACTION.getSatisfaction(), "This event should apply a 10% satisfaction penalty");
 
-        assertEquals(0, GameGlobals.EVENT.getNegativeEvents(), "Check the event is registered as neutral event");
-        assertEquals(0, GameGlobals.EVENT.getPositiveEvents(), "Check the event is registered as neutral event");
+        assertEquals(0, GameGlobals.EVENT.getNegativeEvent(), "Check the event is registered as neutral event");
+        assertEquals(0, GameGlobals.EVENT.getPositiveEvent(), "Check the event is registered as neutral event");
     }
 
 
@@ -158,7 +164,7 @@ public class EventsTest extends TestSuper {
         assertEquals(originalBalance, GameGlobals.MONEY.getBalance(), "This event should have no effect on balance");
         assertEquals(originalSatisfaction, GameGlobals.SATISFACTION.getSatisfaction(), "This event should have no effect on satisfaction");
 
-        assertEquals(1, GameGlobals.EVENT.getNegativeEvents(), "Check the event is registered as negative event");
+        assertEquals(1, GameGlobals.EVENT.getNegativeEvent(), "Check the event is registered as negative event");
     }
 
     @Test
@@ -194,11 +200,8 @@ public class EventsTest extends TestSuper {
         assertEquals(originalBalance+750, GameGlobals.MONEY.getBalance(), "Balance should have increased by £750k (represents insurance payout).");
         assertEquals(originalSatisfaction, GameGlobals.SATISFACTION.getSatisfaction(), "This event should have no effect on satisfaction");
 
-        assertEquals(1, GameGlobals.EVENT.getNegativeEvents(), "Check the event is registered as negative event");
+        assertEquals(1, GameGlobals.EVENT.getNegativeEvent(), "Check the event is registered as negative event");
     }
-
-
-    // ─── Flooding ────────────────────────────────────────────────────────
 
     @Test
     public void testFireEventNoBuildings() {
@@ -217,8 +220,11 @@ public class EventsTest extends TestSuper {
         assertEquals(originalBalance, GameGlobals.MONEY.getBalance(), "This event should have no effect on balance");
         assertEquals(originalSatisfaction, GameGlobals.SATISFACTION.getSatisfaction(), "This event should have no effect on satisfaction");
 
-        assertEquals(1, GameGlobals.EVENT.getNegativeEvents(), "Check the event is registered as negative event");
+        assertEquals(1, GameGlobals.EVENT.getNegativeEvent(), "Check the event is registered as negative event");
     }
+
+
+    // ─── Flooding ────────────────────────────────────────────────────────
 
     @Test
     public void testFloodEventNoBuildings() {
@@ -237,7 +243,7 @@ public class EventsTest extends TestSuper {
         assertEquals(originalBalance, GameGlobals.MONEY.getBalance(), "This event should have no effect on balance");
         assertEquals(originalSatisfaction, GameGlobals.SATISFACTION.getSatisfaction(), "This event should have no effect on satisfaction");
 
-        assertEquals(1, GameGlobals.EVENT.getNegativeEvents(), "Check the event is registered as negative event");
+        assertEquals(1, GameGlobals.EVENT.getNegativeEvent(), "Check the event is registered as negative event");
     }
 
     @Test
@@ -268,23 +274,100 @@ public class EventsTest extends TestSuper {
         assertEquals(originalBalance, GameGlobals.MONEY.getBalance(), "This event should have no effect on balance");
         assertEquals(originalSatisfaction, GameGlobals.SATISFACTION.getSatisfaction(), "This event should have no effect on satisfaction");
 
-        assertEquals(1, GameGlobals.EVENT.getNegativeEvents(), "Check the event is registered as negative event");
+        assertEquals(1, GameGlobals.EVENT.getNegativeEvent(), "Check the event is registered as negative event");
     }
+
+
+    // ─── Roses Event ─────────────────────────────────────────────────────
 
     @Test
     public void testRosesEvent() {
+        BuildingsMap testMap = getTestMap();
+        GameGlobals.BUILDINGS_MAP = testMap; // Necessary since this event depends on the map
 
+        // Add a building for testing
+        assertTrue(addBasicTestBuilding(testMap, BuildingType.RECREATIONAL, validCoords[0][0], validCoords[0][1], true, true));
+
+        Event relevantEvent = new RosesEvent();
+
+        float originalBalance = GameGlobals.MONEY.getBalance();
+        float originalSatisfaction = GameGlobals.SATISFACTION.getSatisfaction();
+
+        GameGlobals.MONEY.earn(testMap.getPlacedBuildings(), EarnSchedule.DAILY);
+
+        float preEventEarnings = GameGlobals.MONEY.getBalance() - originalBalance;
+        
+        relevantEvent.leftRun.run();
+
+        GameGlobals.MONEY.earn(testMap.getPlacedBuildings(), EarnSchedule.DAILY);
+        float postEventEarnings = GameGlobals.MONEY.getBalance() - originalBalance - preEventEarnings;
+
+        assertTrue(postEventEarnings > preEventEarnings, "The recreational buildings should yield more income after the event");
+
+        // Confirm that satisfaction is handled as expected
+        assertEquals(originalSatisfaction, GameGlobals.SATISFACTION.getSatisfaction(), "This event should have no effect on satisfaction");
+
+        assertEquals(1, GameGlobals.EVENT.getPositiveEvent(), "Check the event is registered as a positive event");
     }
 
     @Test
     public void testRosesWinEvent() {
+        BuildingsMap testMap = getTestMap();
+        GameGlobals.BUILDINGS_MAP = testMap; // Necessary since this event depends on the map
 
+        // Add a building for testing
+        assertTrue(addBasicTestBuilding(testMap, BuildingType.RECREATIONAL, validCoords[0][0], validCoords[0][1], true, true));
+
+        Event relevantEvent = new RosesWinEvent();
+
+        float originalBalance = GameGlobals.MONEY.getBalance();
+        float originalSatisfaction = GameGlobals.SATISFACTION.getSatisfaction();
+
+        // Testing that earnings return to normal
+        GameGlobals.MONEY.earn(testMap.getPlacedBuildings(), EarnSchedule.DAILY);
+
+        float preEventEarnings = GameGlobals.MONEY.getBalance() - originalBalance;
+        
+        relevantEvent.leftRun.run();
+
+        GameGlobals.MONEY.earn(testMap.getPlacedBuildings(), EarnSchedule.DAILY);
+        float postEventEarnings = GameGlobals.MONEY.getBalance() - originalBalance - preEventEarnings;
+
+        assertTrue(postEventEarnings < preEventEarnings, "The recreational buildings should return to their normal (lower-earning) state");
+
+        assertEquals(originalSatisfaction+20, GameGlobals.SATISFACTION.getSatisfaction(), "This event should increase satisfaction by 20%");
     }
 
     @Test
     public void testRosesLoseEvent() {
+        BuildingsMap testMap = getTestMap();
+        GameGlobals.BUILDINGS_MAP = testMap; // Necessary since this event depends on the map
 
+        // Add a building for testing
+        assertTrue(addBasicTestBuilding(testMap, BuildingType.RECREATIONAL, validCoords[0][0], validCoords[0][1], true, true));
+
+        Event relevantEvent = new RosesWinEvent();
+
+        float originalBalance = GameGlobals.MONEY.getBalance();
+        float originalSatisfaction = GameGlobals.SATISFACTION.getSatisfaction();
+
+        // Testing that earnings return to normal
+        GameGlobals.MONEY.earn(testMap.getPlacedBuildings(), EarnSchedule.DAILY);
+
+        float preEventEarnings = GameGlobals.MONEY.getBalance() - originalBalance;
+        
+        relevantEvent.leftRun.run();
+
+        GameGlobals.MONEY.earn(testMap.getPlacedBuildings(), EarnSchedule.DAILY);
+        float postEventEarnings = GameGlobals.MONEY.getBalance() - originalBalance - preEventEarnings;
+
+        assertTrue(postEventEarnings < preEventEarnings, "The recreational buildings should return to their normal (lower-earning) state");
+
+        assertEquals(originalSatisfaction-10, GameGlobals.SATISFACTION.getSatisfaction(), "This event should decrease satisfaction by 10%");
     }
+
+    
+    // ─── Sponsorship Event (Free Building) ───────────────────────────────
 
     @Test
     public void testSponsorEvent() {
@@ -304,16 +387,93 @@ public class EventsTest extends TestSuper {
         assertEquals(originalBalance, GameGlobals.MONEY.getBalance(), "This event should have no effect on balance, even after placing a building");
         assertEquals(originalSatisfaction, GameGlobals.SATISFACTION.getSatisfaction(), "This event should have no effect on satisfaction");
 
-        assertEquals(1, GameGlobals.EVENT.getPositiveEvents(), "Check the event is registered as positive event");
+        assertEquals(1, GameGlobals.EVENT.getPositiveEvent(), "Check the event is registered as positive event");
+    }
+
+
+    // ─── Strikes Event ───────────────────────────────────────────────────
+
+    @Test
+    public void testStrikesEventIgnore() {
+        BuildingsMap testMap = getTestMap();
+        GameGlobals.BUILDINGS_MAP = testMap; // Necessary since this event depends on the map
+
+        Event relevantEvent = new StrikesEvent();
+
+        float originalBalance = GameGlobals.MONEY.getBalance();
+        float originalSatisfaction = GameGlobals.SATISFACTION.getSatisfaction();
+        
+        relevantEvent.leftRun.run(); // The left option is where a player choses not to pay off the strikes
+
+        // Confirm that satisfaction is handled as expected
+        assertEquals(originalBalance, GameGlobals.MONEY.getBalance(), "This event should have no effect on balance.");
+        assertEquals(originalSatisfaction-5, GameGlobals.SATISFACTION.getSatisfaction(), "This event should apply a 5% satisfaction penalty");
+
+        // Confirm that a recurring event has been scheduled
+        assertNotNull(GameGlobals.EVENT.getEventQueue().get(GameGlobals.TIME_REMAINING-10), "Confirm that recurring strike event has been scheduled");
+
+        // Confirm that building is not allowed
+        assertFalse(addBasicTestBuilding(testMap, BuildingType.ACADEMIC, validCoords[0][0], validCoords[0][1], false, true));
+
+        assertEquals(1, GameGlobals.EVENT.getNegativeEvent(), "Check the event is registered as negative event");
     }
 
     @Test
-    public void testStrikesEvent() {
+    public void testStrikesEventPayUp() {
+        BuildingsMap testMap = getTestMap();
+        GameGlobals.BUILDINGS_MAP = testMap; // Necessary since this event depends on the map
 
+        // Add a building for testing
+        assertTrue(addBasicTestBuilding(testMap, BuildingType.ACADEMIC, validCoords[0][0], validCoords[0][1], true, true));
+        assertTrue(addBasicTestBuilding(testMap, BuildingType.RECREATIONAL, validCoords[1][0], validCoords[1][1], true, true));
+
+        Event relevantEvent = new StrikesEvent();
+
+        float originalBalance = GameGlobals.MONEY.getBalance();
+        float originalSatisfaction = GameGlobals.SATISFACTION.getSatisfaction();
+
+        GameGlobals.MONEY.earn(testMap.getPlacedBuildings(), EarnSchedule.SEMESTERLY);
+        float preEventEarningsSemesterly = GameGlobals.MONEY.getBalance() - originalBalance;
+
+        GameGlobals.MONEY.earn(testMap.getPlacedBuildings(), EarnSchedule.DAILY);
+        float preEventEarningsDaily = GameGlobals.MONEY.getBalance() - originalBalance - preEventEarningsSemesterly;
+        
+        relevantEvent.rightRun.run(); // The right option is where a player choses to pay off the strikes
+
+        GameGlobals.MONEY.earn(testMap.getPlacedBuildings(), EarnSchedule.SEMESTERLY);
+        float postEventEarningsSemesterly = GameGlobals.MONEY.getBalance() - originalBalance - preEventEarningsSemesterly - preEventEarningsDaily;
+
+        GameGlobals.MONEY.earn(testMap.getPlacedBuildings(), EarnSchedule.DAILY);
+        float postEventEarningsDaily = GameGlobals.MONEY.getBalance() - originalBalance - preEventEarningsSemesterly - preEventEarningsDaily - postEventEarningsSemesterly;
+
+        assertTrue(postEventEarningsDaily < preEventEarningsDaily, "The recreation building should yield less money every day");
+        assertTrue(postEventEarningsSemesterly < preEventEarningsSemesterly, "The academic buildings should now cost more every semester");
+
+        // Confirm that satisfaction is handled as expected
+        assertEquals(originalSatisfaction-5, GameGlobals.SATISFACTION.getSatisfaction(), "This event should apply a 5% satisfaction penalty");
+
+        // Confirm that building is now allowed
+        assertTrue(addBasicTestBuilding(testMap, BuildingType.ACADEMIC, validCoords[2][0], validCoords[2][1], false, true));
     }
 
     @Test
     public void testStrikesResolvedEvent() {
+        BuildingsMap testMap = getTestMap();
+        GameGlobals.BUILDINGS_MAP = testMap; // Necessary since this event depends on the map
 
+        Event relevantEvent = new StrikesResolvedEvent();
+
+        float originalBalance = GameGlobals.MONEY.getBalance();
+        float originalSatisfaction = GameGlobals.SATISFACTION.getSatisfaction();
+
+        // Literally nothing should happen when the award event occurs
+        relevantEvent.leftRun.run();
+        relevantEvent.rightRun.run();
+
+        assertEquals(originalBalance, GameGlobals.MONEY.getBalance(), "This event should have no effect on balance");
+        assertEquals(originalSatisfaction, GameGlobals.SATISFACTION.getSatisfaction(), "This event should have no effect on satisfaction");
+
+        // Confirm that building is now allowed
+        assertTrue(addBasicTestBuilding(testMap, BuildingType.ACADEMIC, validCoords[2][0], validCoords[2][1], false, true));
     }
 }
