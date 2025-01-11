2cf856d218def5d65c6bc10deeb90a70f89dfeac
ldc535 <ldc535@york.ac.uk>
2025-01-11 12:58:06 +0000

    added import

--- core/src/main/java/com/vikingz/unitycoon/events/eventfiles/AwardEvent.java
+++ core/src/main/java/com/vikingz/unitycoon/events/eventfiles/AwardEvent.java
@@ -1,5 +1,7 @@
 package com.vikingz.unitycoon.events.eventfiles;
 
+import com.vikingz.unitycoon.global.GameGlobals;
+
 /**
  * This is a new class which creates an award event object.
  * It was implemented to complete UR_EVENTS.
